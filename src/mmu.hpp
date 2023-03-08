e1d7edf6608ad46287df1ced97167d0f82274504
yzays8 <yzays8@gmail.com>
2023-03-09 02:31:02 +0900

    Replace tabs with spaces

--- src/mmu.hpp
+++ src/mmu.hpp
@@ -7,10 +7,10 @@ class MMU {
  public:
   MMU();
   void LoadFile(uint16_t base_addr, std::string path);
-	uint8_t& ReadByte(uint16_t addr);
-	uint16_t ReadShort(uint16_t addr) const;	// read 16bits
-	void WriteByte(uint16_t addr, uint8_t data);
-	void WriteShort(uint16_t addr, uint16_t data);	// write 16bits
+  uint8_t& ReadByte(uint16_t addr);
+  uint16_t ReadShort(uint16_t addr) const;	// read 16bits
+  void WriteByte(uint16_t addr, uint8_t data);
+  void WriteShort(uint16_t addr, uint16_t data);	// write 16bits
 
  private:
   uint8_t memory_map_[0x10000];
